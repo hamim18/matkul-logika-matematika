277afc58e771447f42afe35516b26a56184971dd
Hamim18 <hamimltf18@gmail.com>
2025-11-03 18:34:13 +0700

    level 2 menambahkan checkbox untuk jumlah emas

--- Hamim-Uts/kalkulatorZakat/zakat-maal.js
+++ Hamim-Uts/kalkulatorZakat/zakat-maal.js
@@ -3,13 +3,33 @@ function hitungZakat() {
     const haul = parseFloat(document.getElementById('haul').value);
     let hasilText = '';
 
-    if (emas >= 85 && haul >= 1) {
-        hasilText = "Wajib Zakat";
-    } else {
-        hasilText = "Tidak Wajib Zakat";
+    if (!isNaN(emas) && !isNaN(haul)) {
+        if (emas >= 85 && haul >= 1) {
+            hasilText = "Wajib Zakat";
+        } else {
+            hasilText = "Tidak Wajib Zakat";
+        }
+    }else {
+        hasilText = "Input tidak valid";
     }
 
     document.getElementById('hasil').innerText = hasilText;
 }
 
 document.getElementById('hitungBtn').addEventListener('click', hitungZakat);
+
+const cekEmas = document.getElementById('cekEmas');
+const inputEmas = document.getElementById('emas');
+
+cekEmas.addEventListener('change', function() {
+    if (this.checked) {
+        inputEmas.disabled = false;
+    } else {
+        inputEmas.disabled = true;
+        inputEmas.value = 0; 
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    inputEmas.disabled = !cekEmas.checked;
+});
