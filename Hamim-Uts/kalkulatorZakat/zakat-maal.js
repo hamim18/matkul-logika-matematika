7b65420f9e453515c8c7cbc019fa935fbe1ddd64
Hamim18 <hamimltf18@gmail.com>
2025-11-03 19:21:15 +0700

    level 4 hitung nominal zakat emas dan argumen logis

--- Hamim-Uts/kalkulatorZakat/zakat-maal.js
+++ Hamim-Uts/kalkulatorZakat/zakat-maal.js
@@ -1,18 +1,27 @@
 function hitungZakat() {
     const emas = parseFloat(document.getElementById('emas').value);
     const haul = parseFloat(document.getElementById('haul').value);
+    const kursEmas = 2321000;
+
+    let nominalText = '';
+    let argumenText = '';
     let hasilText = '';
 
     if (!isNaN(emas) && !isNaN(haul)) {
         if (emas >= 85 && haul >= 1) {
+            const zakatGram = emas * 0.025;
+            const zakatRupiah = zakatGram * kursEmas;
             hasilText = "Wajib Zakat";
+            nominalText = `Nominal zakat yang harus dikeluarkan: Rp ${zakatRupiah.toLocaleString('id-ID')}`;
+            argumenText = `Saat ini Anda diwajibkan mengeluarkan zakat karena memiliki ${emas} gram emas selama ${haul} tahun. Jumlah zakat yang harus dikeluarkan adalah Rp ${zakatRupiah.toLocaleString('id-ID')}`;
         } else {
             hasilText = "Tidak Wajib Zakat";
         }
     }else {
         hasilText = "Input tidak valid";
     }
-
+    document.getElementById('nominalZakat').innerText = nominalText;
+    document.getElementById('argumenLogis').innerText = argumenText;
     document.getElementById('hasil').innerText = hasilText;
 
     const hukum = `
