286c7160ae1948f27b397ebb875eab206d27216a
Hamim18 <hamimltf18@gmail.com>
2025-11-03 16:37:59 +0700

    menambahkan tugas membuat kalkulator zakat

---
+++ Hamim-Uts/kalkulatorZakat/script.js
@@ -0,0 +1,7 @@
+function pilihZakat(jenis) {
+    if (jenis === 'maal') {
+        alert("Anda memilih Zakat Maal.\nPerhitungan berdasarkan nisab dan haul.");
+    } else if (jenis === 'fitrah') {
+        alert("Anda memilih Zakat Fitrah.\nPerhitungan berdasarkan jumlah jiwa dan kebutuhan pokok (beras/gandum).");
+    }
+}
